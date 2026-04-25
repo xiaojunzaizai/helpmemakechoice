1022e91eac43f5dc7760682225e08db8dd172186
xiaojunzaizai <xiaojunzaizai@gmail.com>
2026-04-25 00:38:25 -0400

    code cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	allowedDevOrigins: ['192.168.50.195'],
 };
 
 export default nextConfig;
